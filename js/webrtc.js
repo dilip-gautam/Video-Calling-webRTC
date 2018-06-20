c97c4bb57147e7dfb38ec5293d76d37eb3f71fad
hiddendreamz <[hiddendreamz7@gmail.com]>
2018-06-20 14:02:49 +0545

    Added Username

---
+++ js/webrtc.js
@@ -0,0 +1,89 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBrJ59KMwslQVTGdyoaA-HJvaVVpGU4rCk",
+    authDomain: "webrtc-7a06d.firebaseapp.com",
+    databaseURL: "https://webrtc-7a06d.firebaseio.com",
+    projectId: "webrtc-7a06d",
+    storageBucket: "webrtc-7a06d.appspot.com",
+    messagingSenderId: "492577094946"
+  };
+
+firebase.initializeApp(config);
+
+var database = firebase.database().ref();
+var yourVideo = document.getElementById("yourVideo");
+var friendsVideo = document.getElementById("friendsVideo");
+var callIdField = document.getElementById("callerId");
+let yourId;
+let callId;
+
+function login(form) {
+    if(form.username.value === "" || form.username.value === undefined){
+        alert("Enter valid username");
+        return false;
+    }
+	yourId = form.username.value;
+    form.username.value = yourId + "(logged in)";
+    form.username.disabled = true;
+    form.login_submit.disabled = true;
+    return false; //
+}
+
+var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, 
+                {'urls': 'stun:stun.l.google.com:19302'},
+                {'urls': 'turn:numb.viagenie.ca', 'credential': 'Tempo12345','username': 'hiddendreamz7@gmail.com'}]};
+
+function sendMessage(senderId, receiverId, data) {
+    var msg = database.push({
+        sender: senderId,
+        receiver: receiverId,
+        message: data
+    });
+    msg.remove();
+}
+
+function showMyFace() {
+    navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true
+    }).then(stream => yourVideo.srcObject = stream)
+        .then(stream => pc.addStream(stream));
+}
+
+var pc = new RTCPeerConnection(servers);
+pc.onicecandidate = (event => event.candidate ?
+    sendMessage(yourId, callId, JSON.stringify({
+        'ice': event.candidate
+    })): console.log("Sent All Ice"));
+pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
+
+function showFriendsFace() {
+    callId = callIdField.value;
+    pc.createOffer()
+        .then(offer => pc.setLocalDescription(offer))
+        .then(() => sendMessage(yourId, callId, JSON.stringify({
+            'sdp': pc.localDescription
+        })));
+    return false;
+}
+
+function readMessage(data) {
+    var msg = JSON.parse(data.val().message);
+    var sender = data.val().sender;
+    var receiver = data.val().receiver;
+    console.log("Message from " + sender + " to " + receiver);
+    if (receiver === yourId) {
+        if (msg.ice !== undefined)
+            pc.addIceCandidate(new RTCIceCandidate(msg.ice));
+        else if (msg.sdp.type === "offer")
+            pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+                .then(() => pc.createAnswer())
+                .then(answer => pc.setLocalDescription(answer))
+                .then(() => sendMessage(yourId, sender, JSON.stringify({
+                    'sdp': pc.localDescription
+                })));
+        else if (msg.sdp.type === "answer")
+            pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+    }
+}
+database.on('child_added', readMessage);
